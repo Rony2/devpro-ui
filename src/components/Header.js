9fcccc6e82202ceb910ca032f2bc118d1354174c
Ronak Patel <ronapatelg5@gmail.com>
2024-10-23 23:27:02 +0530

    feat/quiz-page - redirection to quiz page and css fix for header icon and title

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,16 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center p-4 text-black border-b">
       <div className="flex items-center">
-        <Image
-          src="/logo.svg"
-          alt="Logo"
-          className="mr-2"
-          height={30}
-          width={30}
-        />
-        <h1 className="text-xl">DevPro</h1>
+        <Link href="/" className="flex">
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            className="mr-2"
+            height={30}
+            width={30}
+          />
+          <h1 className="text-xl">DevPro</h1>
+        </Link>
         <Link href='/quiz'>
           <p className="ml-4"> Practise Questions</p>
         </Link>
