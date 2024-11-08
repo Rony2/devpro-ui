4385d53aca8bd95b320f9566a5368ab121690225
Ronak Patel <ronapatelg5@gmail.com>
2024-11-09 01:28:20 +0530

    fix: margin top removed from footer text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className="">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col items-center border-t border-slate-400/10 py-4 sm:justify-between">
-                    <p className="mt-6 text-sm text-slate-500 mt-0">Copyright ©2024 DevPro. All rights reserved.</p>
+                    <p className="text-sm text-slate-500 mt-0">Copyright ©2024 DevPro. All rights reserved.</p>
                 </div>
             </div>
         </footer>
